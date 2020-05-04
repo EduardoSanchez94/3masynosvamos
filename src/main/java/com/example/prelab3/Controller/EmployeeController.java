508f66b9866d0e9c744de29b4d812819ab8d9e63
Gabrielcv98 <gabrialbert98@gmail.com>
2020-05-03 21:41:42 -0500

    sin employee sin manager

--- src/main/java/com/example/prelab3/Controller/EmployeeController.java
+++ src/main/java/com/example/prelab3/Controller/EmployeeController.java
@@ -1,9 +1,32 @@
 package com.example.prelab3.Controller;
 
+import com.example.prelab3.Entity.Employee;
+import com.example.prelab3.Entity.Jobs;
+import com.example.prelab3.Repository.EmployeeRepository;
+import com.example.prelab3.Repository.JobsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 
+@RequestMapping("/Employee")
 public class EmployeeController {
+@Autowired
+    EmployeeRepository employeeRepository;
+    @GetMapping("")
+    public String primera(){
+        return "index";
+    }
+    @GetMapping("/listaEmployee")
+    public String listaJobs(Model model){
+        List<Employee> listaEmployee = employeeRepository.findAll();
+        model.addAttribute("listaEmployee",listaEmployee);
+        return "Gabriel/listaEmployee";
+    }
 
 }
--- src/main/java/com/example/prelab3/Repository/EmployeeRepository.java
+++ src/main/java/com/example/prelab3/Repository/EmployeeRepository.java
@@ -1,4 +1,9 @@
 package com.example.prelab3.Repository;
 
-public interface EmployeeRepository extends {
+import com.example.prelab3.Entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,String> {
 }

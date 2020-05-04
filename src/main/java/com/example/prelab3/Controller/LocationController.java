238e2247d39f339df914daa2842b213895e08a17
Josue Romero <jcromero@pucp.edu.pe>
2020-05-03 20:13:54 -0500

    josue 3

---
+++ src/main/java/com/example/prelab3/Controller/LocationController.java
@@ -0,0 +1,14 @@
+package com.example.prelab3.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LocationController {
+
+
+
+        public String lista(){
+
+            return "Josue/lista";
+        }
+}

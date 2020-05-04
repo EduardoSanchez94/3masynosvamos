dfbfaf493e255cfd92ed216a788ab8115b0ec055
Josue Romero <jcromero@pucp.edu.pe>
2020-05-03 20:19:51 -0500

    josu tu papi mas na

--- src/main/java/com/example/prelab3/Controller/LocationController.java
+++ src/main/java/com/example/prelab3/Controller/LocationController.java
@@ -1,14 +1,27 @@
 package com.example.prelab3.Controller;
 
+import com.example.prelab3.Entity.Location;
+import com.example.prelab3.Repository.CountryRepo;
+import com.example.prelab3.Repository.LocationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import javax.jws.WebParam;
+import java.util.List;
 
 @Controller
 public class LocationController {
 
+    @Autowired
+    CountryRepo countryRepo;
+    @Autowired
+    LocationRepository locationRepository;
 
+    public String lista(Model model) {
 
-        public String lista(){
-
-            return "Josue/lista";
-        }
+        List<Location> listaLocation = locationRepository.findAll();
+        model.addAttribute("lista", listaLocation);
+        return "Josue/lista";
+    }
 }

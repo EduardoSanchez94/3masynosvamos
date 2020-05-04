d92163aa96fc72b10482b9e6e3c8aedb1ad6d948
Josue Romero <jcromero@pucp.edu.pe>
2020-05-03 20:31:07 -0500

    josue location list

--- src/main/java/com/example/prelab3/Controller/LocationController.java
+++ src/main/java/com/example/prelab3/Controller/LocationController.java
@@ -6,11 +6,14 @@ import com.example.prelab3.Repository.LocationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.jws.WebParam;
 import java.util.List;
 
 @Controller
+@RequestMapping("/location")
 public class LocationController {
 
     @Autowired
@@ -18,10 +21,12 @@ public class LocationController {
     @Autowired
     LocationRepository locationRepository;
 
+
+    @GetMapping("")
     public String lista(Model model) {
 
         List<Location> listaLocation = locationRepository.findAll();
         model.addAttribute("lista", listaLocation);
-        return "Josue/lista";
+        return "Josue/josue";
     }
 }
